63f1127be83229cff18d3d276bbfcc71f1de958a
Rahul Shah <rahul@n174-p199.eduroam.kth.se>
2019-02-19 14:53:15 +0100

    Edited code

--- Lab2.py
+++ Lab2.py
@@ -1,17 +1,28 @@
-import numpy , random , math 
+import numpy as np
+import random
+import math
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
-numpy.random.seed(100)
-classA = numpy.concatenate ( 
-	(numpy.random.randn(10, 2) ∗ 0.2 + [1.5, 0.5],
-	 numpy.random.randn(10, 2) ∗ 0.2 + [−1.5, 0.5]))
-classB = numpy.random.randn(20, 2) ∗ 0.2 + [0.0, −0.5]
 
-inputs = numpy.concatenate((classA, classB))
-targets = numpy.concatenate(
-	(numpy.ones(classA.shape[0]),
-	 −numpy.ones(classB.shape[0])))
+def kerlel_linear():
+	return
+
+def objective(alphas):
+	ret = 0
+	for i in range(len(alphas)):
+		for j in range(len(alphas)):
+			ret = ret + alphas[i] * alphas[j] * targets[i] * targets[j]
+
+	ret = ret / 2 - sum (alphas)
+	return ret
+
+np.random.seed(100)
+classA = np.concatenate((np.random.randn(10, 2) * 0.2 + [1.5, 0.5], np.random.randn(10, 2) * 0.2 + [−1.5, 0.5]))
+classB = np.random.randn(20, 2) * 0.2 + [0.0, −0.5]
+
+inputs = np.concatenate((classA, classB))
+targets = np.concatenate(((np.ones(classA.shape[0])), −np.ones(classB.shape[0])))
 
 N = inputs.shape[0] # Number of rows (samples)
 
@@ -21,18 +32,6 @@ inputs = inputs[permute, :]
 targets = targets[permute]
 
 
-def kerlel_linear():
-	return 
-
-def objective(alphas):
-	ret = 0
-	for i in range(len(alphas)):
-		for j in range(len(alphas)):
-			ret = ret + alphas[i] * alphas[j] 
-				  * targets[i] * targets[j]
-
-	ret = ret / 2 - sum (alphas)
-	return ret
 
 start = numpy.zeros(N) #Initial guess of the aöpha-vector
 
