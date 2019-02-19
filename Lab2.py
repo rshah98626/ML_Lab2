3d8c90ea9adf0e2d80424c11b5d936080e0359a6
Rahul Shah <rahul@n174-p199.eduroam.kth.se>
2019-02-19 15:59:38 +0100

    Stuff

--- Lab2.py
+++ Lab2.py
@@ -5,18 +5,17 @@ from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
 np.random.seed(100)
-classA = np.concatenate((np.random.randn(10, 2) * 0.2 + [1.5, 0.5], np.random.randn(10, 2) * 0.2 + [−1.5, 0.5]))
-classB = np.random.randn(20, 2) * 0.2 + [0.0, −0.5]
-
-inputs = np.concatenate((classA, classB))
-targets = np.concatenate(((np.ones(classA.shape[0])), −np.ones(classB.shape[0])))
+classA = np.concatenate( (np.random.randn(10, 2) * 0.2 + [1.5, 0.5], np.random.randn(10, 2) * 0.2 + [-1.5, .5]) )
+classB = np.random.randn(20, 2) * 0.2 + [0.0, -0.5]
+inputs = np.concatenate (( classA , classB ))
+targets = np.concatenate(( np.ones(classA.shape[0]), -np.ones(classB.shape[0]) ))
 
 N = inputs.shape[0] # Number of rows (samples)
-C = 1
+C = .03
 
 permute = list(range(N))
 random.shuffle(permute)
-inputs = inputs[permute, :]
+inputs = inputs[permute , :]
 targets = targets[permute]
 
 def kernel_linear(x, y):
@@ -32,31 +31,36 @@ def objective(alphas):
 	alph = 0
 	for i, ai in enumerate(alphas):
 		for j, aj in enumerate(alphas):
-			alph += ai * aj * targets[i] * targets[j] * kernel_linear(inputs[i], inputs[j])
+			alph += ai * aj * targets[i] * targets[j] * kernel_polynomial(inputs[i], inputs[j], 4)
 
 	alph /= 2
 	alph -= sum(alphas)
 	return alph
 
-def zerofun(a):
+def zerofun(alpha):
+	tot = 0
+	for i, ai in enumerate(alpha):
+		tot += i * ai
+	return tot
 
 
-start = numpy.zeros(N) #Initial guess of the alpha-vector
+start = np.zeros(N) #Initial guess of the alpha-vector
 B = [(0, C) for b in range(N)]
 XC = {'type': 'eq', 'fun': zerofun}
 ret = minimize (objective, start, bounds=B, constraints=XC)
-alpha = ret [’x’]
+alpha = ret['x']
+print(alpha)
 
 
 
 
 
-plt.plot([p[0] for p in classA],
-		 [p[1] for p in classA],
-		 ’b. ’)
-plt.plot([p[0] for p in classB],
-		 [p[1] for p in classB],
-		 ’r. ’)
-plt.axis(’equal’) #Force same scale on both axes
-plt.savefig(’svmplot.pdf’) #Save a copy in a file
-plt.show() #Show the plot on the screen
+# plt.plot([p[0] for p in classA],
+# 		 [p[1] for p in classA],
+# 		 ’b. ’)
+# plt.plot([p[0] for p in classB],
+# 		 [p[1] for p in classB],
+# 		 ’r. ’)
+# plt.axis(’equal’) #Force same scale on both axes
+# plt.savefig(’svmplot.pdf’) #Save a copy in a file
+# plt.show() #Show the plot on the screen
