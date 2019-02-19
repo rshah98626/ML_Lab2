0d0afd5027e83c101dcc6940ad8c3b4ab5e7c4d7
Rahul Shah <rahul@n174-p199.eduroam.kth.se>
2019-02-19 15:27:24 +0100

    Testing

--- Lab2.py
+++ Lab2.py
@@ -4,22 +4,6 @@ import math
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 
-
-def kernel_linear(x, y):
-	return np.transpose(x) @ y
-
-def kernel_polynomial(x, y, degree):
-	return (np.transpose(x) @ y + 1) ** degree
-
-def objective(alphas):
-	ret = 0
-	for i in range(len(alphas)):
-		for j in range(len(alphas)):
-			ret = ret + alphas[i] * alphas[j] * targets[i] * targets[j]
-
-	ret = ret / 2 - sum (alphas)
-	return ret
-
 np.random.seed(100)
 classA = np.concatenate((np.random.randn(10, 2) * 0.2 + [1.5, 0.5], np.random.randn(10, 2) * 0.2 + [−1.5, 0.5]))
 classB = np.random.randn(20, 2) * 0.2 + [0.0, −0.5]
@@ -28,16 +12,38 @@ inputs = np.concatenate((classA, classB))
 targets = np.concatenate(((np.ones(classA.shape[0])), −np.ones(classB.shape[0])))
 
 N = inputs.shape[0] # Number of rows (samples)
+C = 1
 
 permute = list(range(N))
 random.shuffle(permute)
 inputs = inputs[permute, :]
 targets = targets[permute]
 
+def kernel_linear(x, y):
+	return np.transpose(x) @ y
+
+def kernel_polynomial(x, y, degree):
+	return (np.transpose(x) @ y + 1) ** degree
+
+def kernel_RBF(x, y, sigma):
+	return math.exp( -np.linalg.norm(x-y, 2) / (2*(sigma**2)) )
+
+def objective(alphas):
+	alph = 0
+	for i, ai in enumerate(alphas):
+		for j, aj in enumerate(alphas):
+			alph += ai * aj * targets[i] * targets[j] * kernel_linear(inputs[i], inputs[j])
+
+	alph /= 2
+	alph -= sum(alphas)
+	return alph
+
+def zerofun(a):
 
 
-start = numpy.zeros(N) #Initial guess of the aöpha-vector
-
+start = numpy.zeros(N) #Initial guess of the alpha-vector
+B = [(0, C) for b in range(N)]
+XC = {'type': 'eq', 'fun': zerofun}
 ret = minimize (objective, start, bounds=B, constraints=XC)
 alpha = ret [’x’]
 
