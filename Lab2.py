310063e5ccd7f7d40f9fca9ef304c1522235a947
Martin <m.kjaeer@posteo.at>
2019-02-20 20:17:42 +0100

    Calculating bias, indicator & plotting decision boundary

--- Lab2.py
+++ Lab2.py
@@ -3,29 +3,34 @@ import random
 import math
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+from tabulate import tabulate
 
-#np.random.seed(100)
-classA = np.concatenate( (np.random.randn(10, 2) * 0.2 + [1.5, 0.5], np.random.randn(10, 2) * 0.2 + [-1.5, .5]) )
+np.random.seed(100)
+classA = np.concatenate((np.random.randn(10, 2) * 0.2 + [1.5, 0.5], np.random.randn(10, 2) * 0.2 + [-1.5, .5]))
 classB = np.random.randn(20, 2) * 0.2 + [0.0, -0.5]
-inputs = np.concatenate (( classA , classB ))
-targets = np.concatenate(( np.ones(classA.shape[0]), -np.ones(classB.shape[0]) ))
+inputs = np.concatenate ((classA, classB))
+targets = np.concatenate((np.ones(classA.shape[0]), -np.ones(classB.shape[0])))
 
-N = inputs.shape[0] # Number of rows (samples)
-C = .03
+N = inputs.shape[0]  # Number of rows (samples)
+C = 3.0
 
 permute = list(range(N))
 random.shuffle(permute)
-inputs = inputs[permute , :]
+inputs = inputs[permute, :]
 targets = targets[permute]
+
 
 def kernel_linear(x, y):
 	return np.transpose(x) @ y
 
+
 def kernel_polynomial(x, y, degree):
 	return (np.transpose(x) @ y + 1) ** degree
 
+
 def kernel_RBF(x, y, sigma):
 	return math.exp( -np.linalg.norm(x-y, 2) / (2*(sigma**2)) )
+
 
 def objective(alphas):
 	alph = 0
@@ -37,52 +42,65 @@ def objective(alphas):
 	alph -= sum(alphas)
 	return alph
 
-def zerofun(alpha):
-	tot = 0
-	for i, ai in enumerate(alpha):
-		tot += targets[i] * ai
-	return tot
 
+def zerofun(alphas):
+	return np.transpose(targets) @ alphas
 
-start = np.zeros(N) # Initial guess of the alpha-vector
+start = np.zeros(N)  # Initial guess of the alpha-vector
 B = [(0, C) for b in range(N)]
 XC = {'type': 'eq', 'fun': zerofun}
-ret = minimize (objective, start, bounds=B, constraints=XC)
+ret = minimize(objective, start, bounds=B, constraints=XC)
 alpha = ret['x']
 
-success = ret ['success']
-if (success):
-	print("Minimize function was successfull :)")
+if ret['success']:
+	print("Minimize function was successfull :)\n")
 else:
-	print("Minimize function was not successfull :(")
-print(alpha)
+	print("Minimize function was not successfull :(\n")
 
 non_zero_alpha = []
 for index, value in enumerate(alpha):
 	if value > 10 ** (-5):
 		non_zero_alpha.append((inputs[index], targets[index], value))
 
+print(tabulate(non_zero_alpha, headers=['input', 'target', 'alpha']), "\n")
+
 # Bias calculation
+# First: find support vector. This "corresponds to any point with an α-value
+# larger than zero, but less than C"
+sv = 0
+for i, entry in enumerate(non_zero_alpha):
+	if entry[2] < C:
+		sv = i
+		break
+# Now calulate bias:
 bias = 0
 for entry in non_zero_alpha:
-	for i, ai in enumerate(alpha):
-		bias += ai * targets[i] * kernel_linear(non_zero_alpha[0][0], inputs[i])
-bias -= non_zero_alpha[0][1]
+	bias += entry[2] * entry[1] * kernel_linear(non_zero_alpha[sv][0], entry[0])
+bias -= non_zero_alpha[sv][1]
+print("Calculated bias:", bias)
 
-#xgrid = np.linspace(−5, 5)
-#ygrid = np.linspace(−4, 4)
 
-#grid = np.array([[indicator(x, y) for x in xgrid] for y in ygrid])
+def indicator(x, y):
+	ind = 0
+	for entry in non_zero_alpha:
+		ind += entry[2] * entry[1] * kernel_linear([x, y], entry[0])
+	ind -= bias
+	return ind
 
+
+# Plotting:
 plt.plot([p[0] for p in classA],
-		 [p[1] for p in classA],
-		 'b.')
+		[p[1] for p in classA],
+		'b.')
 plt.plot([p[0] for p in classB],
-		 [p[1] for p in classB],
-		 'r.')
-plt.axis('equal') #Force same scale on both axes
+		[p[1] for p in classB],
+		'r.')
+plt.axis('equal')  # Force same scale on both axes
 
-#plt.contour(xgrid, ygrid, grid, (−1.0, 0.0, 1.0), colors = ('red', 'black', 'blue'), linewidths = (1, 3, 1))
+xgrid = np.linspace(-5, 5)
+ygrid = np.linspace(-4, 4)
+grid = np.array([[indicator(x, y) for x in xgrid] for y in ygrid])
+plt.contour(xgrid, ygrid, grid, (-1, 0, 1), colors=('red', 'black', 'blue'), linewidths=(1, 3, 1))
 
-plt.savefig('svmplot.pdf') #Save a copy in a file
-plt.show() #Show the plot on the screen
+plt.savefig('svmplot.pdf')  #  Save a copy in a file
+plt.show()  # Show the plot on the screen
